a43415bd32057f3985acb3ecd96f09c746deea09
Noah Burghardt <burghardtnoah@gmail.com>
2024-12-06 07:20:57 -0700

    hey man if it works, it works

---
+++ 06.py
@@ -0,0 +1,97 @@
+import lib
+
+def turn_right(direction):
+    return {
+        (0, 1): (1, 0),
+        (1, 0): (0, -1),
+        (0, -1): (-1, 0),
+        (-1, 0): (0, 1),
+    }[direction]
+
+
+def p1():
+    data = lib.read()
+
+    grid = lib.Grid([list(i) for i in data.split('\n')])
+    pos = grid.find('^')
+    assert pos is not None
+    direction = (0, 1)
+    visited = set()
+    visited_vectors = set()
+    visited_vectors.add(pos + direction)
+    visited.add(pos)
+
+    while True:
+        next = (pos[0] + direction[0], pos[1] + direction[1])
+        next_value = grid.cell(next[0], next[1])
+        if next_value is None:
+            break
+        if next_value == '#':
+            direction = turn_right(direction)
+            next = (pos[0] + direction[0], pos[1] + direction[1])
+            next_value = grid.cell(next[0], next[1])
+            if next_value is None:
+                break
+            if next_value == '#':
+                direction = turn_right(direction)
+                next = (pos[0] + direction[0], pos[1] + direction[1])
+
+        pos = next
+
+        current = pos + direction
+        if current in visited_vectors:
+            break
+        visited_vectors.add(current)
+        visited.add(pos)
+
+
+    print(len(visited))
+    
+
+def p2():
+    data = lib.read()
+    grid = lib.Grid([list(i) for i in data.split('\n')])
+    start = grid.find('^')
+    assert start is not None
+    start_dir = (0, 1)
+
+    def has_loop():
+        pos = start
+        direction = start_dir
+        visited_vectors = set()
+        visited_vectors.add(pos + direction)
+
+        while True:
+            next = (pos[0] + direction[0], pos[1] + direction[1])
+            next_value = grid.cell(next[0], next[1])
+            if next_value is None:
+                return False
+            if next_value == '#':
+                direction = turn_right(direction)
+                next = (pos[0] + direction[0], pos[1] + direction[1])
+                next_value = grid.cell(next[0], next[1])
+                if next_value is None:
+                    return False
+                if next_value == '#':
+                    direction = turn_right(direction)
+                    next = (pos[0] + direction[0], pos[1] + direction[1])
+
+            pos = next
+
+            current = pos + direction
+            if current in visited_vectors:
+                return True
+            visited_vectors.add(current)
+
+    s = 0
+    for pos, value in grid.enumerate():
+        if value != '.':
+            continue
+        grid[pos] = '#'
+        if has_loop():
+            s += 1
+        grid[pos] = '.'
+
+    print(s)
+
+p2()
--- lib.py
+++ lib.py
@@ -1,3 +1,6 @@
+from typing import TypeVar, Generic, Generator
+T = TypeVar('T')
+
 def read():
     lines = []
     with open('input.txt') as f:
@@ -8,3 +11,38 @@ def read():
 
 def flatten(l):
     return [i for j in l for i in j]
+
+class Grid(Generic[T]):
+    def __init__(self, cells: list[list[T]]):
+        self._width = len(cells[0])
+        self._height = len(cells)
+        self._cells: list[list[T]] = cells
+
+    def cell(self, x: int, y: int) -> None | T:
+        if self.has_cell(x, y):
+            return self._cells[self._height - 1 - y][x]
+        return None
+
+    def __getitem__(self, xy: tuple[int, int]):
+        return self.cell(xy[0], xy[1])
+
+    def __setitem__(self, xy: tuple[int, int], value: T):
+        self._cells[self._height - 1 - xy[1]][xy[0]] = value 
+
+
+    def enumerate(self) -> Generator[tuple[tuple[int, int], T], None, None]:
+        for y in range(self._height):
+            for x in range(self._width):
+                yield ((x, y), self._cells[self._height - 1 - y][x])
+
+    def has_cell(self, x: int, y: int) -> bool:
+        return y >= 0 and y < self._height and x >= 0 and x < self._width
+
+    def find(self, value: T) -> None | tuple[int, int]:
+        for y in range(self._height):
+            for x in range(self._width):
+                if self.cell(x, y) == value:
+                    return (x, y)
+        return None
+
+
